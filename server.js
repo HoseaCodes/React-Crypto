e7c4a41edf59f09cc0f5886bf219860347900826
hoseacodes <dominique11h@yahoo.com>
2021-02-28 17:23:08 -0600

    Intiaal Commit Heroku

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'build')));
 
 //Routes
 
-app.get('/', (req, res) => {
-    res.json({ msg: "Welcome" })
-})
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
