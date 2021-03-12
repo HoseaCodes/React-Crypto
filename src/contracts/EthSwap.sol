693a4a4d85df9afc46f99479c2ba99d5408a8c6f
hoseacodes <dominique11h@yahoo.com>
2021-03-12 09:16:57 -0600

    Completed Smart Contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,34 +1,56 @@
 pragma solidity ^0.5.0;
 
-import './Token.sol';
+import "./Token.sol";
 
 contract EthSwap {
-   string public name = "EthSwap Instant Exchange";
-   Token public token;
-   uint public rate = 100;
+    string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint256 public rate = 100;
 
-   event TokenPurchased(
-      address account,
-      address token,
-      uint amount,
-      uint rate
-   );
+    event TokensPurchased(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+    event TokensSold(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
 
-   constructor(Token _token) public {
-      token = _token;
-   }
+    constructor(Token _token) public {
+        token = _token;
+    }
 
-   function buyTokens() public payable {
-      //Redemption rate = # of tokens they receive for 1 ether
-      //Amount of Ethereum * Redemption rate
-      //Calculate the number of tokens to buy
-      uint tokenAmount = msg.value * rate;
+    function buyTokens() public payable {
+        //Redemption rate = # of tokens they receive for 1 ether
+        //Amount of Ethereum * Redemption rate
+        //Calculate the number of tokens to buy
+        uint256 tokenAmount = msg.value * rate;
 
-      require(token.balanceOf(address(this)) >= tokenAmount);
+        require(token.balanceOf(address(this)) >= tokenAmount);
 
-      token.transfer(msg.sender, tokenAmount);
+        token.transfer(msg.sender, tokenAmount);
 
-      // Emit an event
-      emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
-   }
+        // Emit an event
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint256 _amount) public {
+        //User can't sell more tokens than they have
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        //Calculate the amount of Ether to redeem
+        uint256 etherAmount = _amount / rate;
+
+        require(address(this).balance >= etherAmount);
+
+        //Perform Sale
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        emit TokensSold(msg.sender, address(token), _amount, rate);
+    }
 }
