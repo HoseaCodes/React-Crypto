4441068ae2633b5ddcad266d318dad6a181be4b3
hoseacodes <dominique11h@yahoo.com>
2021-03-16 11:14:05 -0500

    UX

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, NavItem } from 'react-bootstrap';
 import Logo from '../images/cryptologo.png'
+import Identicon from 'identicon.js';
 import TokenExchange from '../pages/exchange/tokenExchange';
 // import Keys from './keyCount';
 import './style.css';
 
-export default function NavBar() {
+export default function NavBar(props) {
     return (
         <header>
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand className="logo" href="#home">
                     <img src={Logo} alt="Logo" width="100px" />
                     <h1>React-Crypto</h1>
+                    <NavItem>
+                        {props.account}
+                    </NavItem>
+                    <NavItem>
+                        {
+                            props.accounnt
+                                ? <img
+                                    className="ml-2"
+                                    width="30"
+                                    height="30"
+                                    src={`data:image/png;base64,${new Identicon(props.account, 30).toString()}`}
+                                    alt=''
+                                />
+                                : <span></span>
+                        }
+                    </NavItem>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className=" nav-content justify-content-end">
-                    <a href="/">Home</a>
-                    <a href="/tokenexchange">Exchange</a>
+                    <NavItem>
+                        <a href="/">Home</a>
+                    </NavItem>
+                    <NavItem>
+                        <a href="/tokenexchange">Exchange</a>
+                    </NavItem>
                     {/* <Keys /> */}
                 </Navbar.Collapse>
             </Navbar>
--- src/pages/exchange/tokenExchange.jsx
+++ src/pages/exchange/tokenExchange.jsx
@@ -1,13 +1,93 @@
 import React, { Component } from 'react';
+import Web3 from 'web3'
+import EthSwap from '../../abis/EthSwap.json'
+import Token from '../../abis/Token.json'
 import NavBar from '../../components/navBar';
+import Swap from '../../components/swap';
 import logo from '../../images/cryptologo.png';
 import './tokenExchange.css';
 
 class TokenExchange extends Component {
+
+    async componentWillMount() {
+        await this.loadWeb3()
+        await this.loadLockchainData()
+        console.log(window.web3)
+    }
+
+    async loadLockchainData() {
+        const web3 = window.web3
+        const accounts = await web3.eth.getAccounts()
+        this.setState({ account: accounts[0] })
+
+        const ethBalance = await web3.eth.getBalance(this.state.account)
+        this.setState({ ethBalance: ethBalance })
+
+        const networkId = await web3.eth.net.getId()
+        const tokenData = Token.networks[networkId]
+        if (tokenData) {
+            const token = new web3.eth.Contract(Token.abi, tokenData.address)
+            this.setState({ token: token })
+            let tokenBalance = await token.methods.balanceOf(this.state.account).call()
+            this.setState({ tokenBalance: tokenBalance.toString() })
+        } else {
+            window.alert('Token contract not deployed to detected network')
+        }
+        const ethSwapData = EthSwap.networks[networkId]
+        if (ethSwapData) {
+            const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+            this.setState({ ethSwap: ethSwap })
+        } else {
+            window.alert('EthSwap contract not deployed to detected network')
+        }
+
+        this.setState({ loading: false })
+    }
+
+    async loadWeb3() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+            window.alert('Non-Etherum broswer detected. You should consider trying MetaMask')
+        }
+    }
+
+    buyTokens = (etherAmount) => {
+        this.setState({ loading: true })
+        this.state.ethSwap.methods.buyTokens().send({ value: etherAmount, from: this.state.account }).on('transactionHash', (hash) => {
+            this.setState({ loading: false })
+        })
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            account: '',
+            ethBalance: 0,
+            token: {},
+            ethSwap: {},
+            tokenBalance: 0,
+            loading: true
+        }
+    }
     render() {
+        let content
+        if (this.state.loading) {
+            content = <p id="loader">Loading...</p>
+        } else {
+            content = <Swap
+                ethBalance={this.state.ethBalance}
+                tokenBalance={this.state.tokenBalance}
+                buyTokens={this.buyTokens} />
+        }
         return (
             <div>
-                <NavBar />
+                <NavBar account={this.state.account} />
                 <div className="container-fluid mt-5">
                     <div className="row">
                         <main role="main" className="col-lg-12 d-flex text-center">
@@ -19,18 +99,8 @@ class TokenExchange extends Component {
                                 >
                                     <img src={logo} className="App-logo" alt="logo" />
                                 </a>
-                                <h1>Dapp University Starter Kit</h1>
-                                <p>
-                                    Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                                <a
-                                    className="App-link"
-                                    href="http://www.dappuniversity.com/bootcamp"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                                </a>
+                                <h1>Token Exchange</h1>
+                                {content}
                             </div>
                         </main>
                     </div>
