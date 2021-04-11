646acf65100e40ab4941e45e225e77bde4a6c4dc
hoseacodes <dominique11h@yahoo.com>
2021-04-11 16:57:54 -0500

    Added Color NFT

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
+import React, { Component } from 'react'
 import './App.css';
+import Web3 from "web3";
 import Home from './pages/home/home';
 import Error from './pages/error/error';
 import { DataProvider } from './GlobalState';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import TokenExchange from './pages/exchange/tokenExchange';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <DataProvider>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <Home
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/tokenexchange"
-            render={() => (
-              <TokenExchange
-              />
-            )}
-          />
-          <Error />
-        </Switch>
-      </DataProvider>
-    </BrowserRouter>
-  );
+class App extends Component {
+  async componentWillMount() {
+    await this.loadBlockchainData();
+  }
+
+  async loadBlockchainData(dispatch) {
+    if (typeof window.ethereum !== "undefined") {
+      const web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert("Please login with MetaMask");
+    }
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <DataProvider>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <Home
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/tokenexchange"
+              render={() => (
+                <TokenExchange
+                  loadBlockchainData={this.loadBlockchainData}
+                />
+              )}
+            />
+            <Error />
+          </Switch>
+        </DataProvider>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
